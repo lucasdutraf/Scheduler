a54c43457e4837dae873396cd01d804cd46fe3e4
Lucas Dutra <ldutra98@gmail.com>
2019-02-18 16:20:37 -0300

    Improve listview performance

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -4,18 +4,21 @@ class Tasks extends StatelessWidget {
   final List<String> tasks;
   Tasks(this.tasks);
 
+  Widget _buildTaskItem (BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Text(tasks[index])
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: tasks
-      .map((element) => Card(
-        child: Column(
-          children: <Widget>[
-            Text(element)
-          ],
-        ),
-      ))
-      .toList(),
+    return ListView.builder(
+      itemBuilder: _buildTaskItem,
+      itemCount: tasks.length,
     );
   }
 }
